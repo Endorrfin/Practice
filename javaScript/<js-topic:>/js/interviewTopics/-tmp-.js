1c8e8ed2ab9a515fdd5f0da66fa60f2408937b81
Vasyl Krupka <krupka.ua@gmail.com>
2019-12-27 04:27:22 +0200

    <js-topic/>

---
+++ javaScript/<js-topic:>/js/interviewTopics/-tmp-.js
@@ -0,0 +1,11 @@
+// 'use strict';
+
+/**
+|--------------------------------------------------
+  === <<<--- currying--->>>  ===
+  https://www.youtube.com/watch?v=lfNgrsBV9nI&list=PLwHvxJae2LazDrHm6ayqLKz6jszEn7ArQ&index=7
+
+|--------------------------------------------------
+*/
+
+// // === <<<--- Option I --->>>  ===
